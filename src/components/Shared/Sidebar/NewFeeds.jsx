8b616bc1cf868a7a65e33f170b24aeb588a14e7d
Sukanta Das <arnobh84@gmail.com>
2022-12-28 19:22:05 +0600

    new feed completed

--- src/components/Shared/Sidebar/NewFeeds.jsx
+++ src/components/Shared/Sidebar/NewFeeds.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import newsFeed from "../../../Images/Icons/newsfeed.png";
 import badge from "../../../Images/Icons/award.png";
 import storyIcon from "../../../Images/Icons/instagram-stories.png";
+import popularIcon from "../../../Images/Icons/flash-sale.png";
+import userIcon from "../../../Images/Icons/user.png";
 const NewFeeds = () => {
   return (
     <>
-      <div className="shadow-md mt-10 rounded-lg p-3 mx-2">
+      <div className="shadow-md mt-10 rounded-lg p-3 mx-2 bg-white">
         <div>
-          <p className="text-xs text-gray-500 font-bold">New Feeds</p>
+          <p className="text-sm text-gray-500 font-bold">New Feeds</p>
           <div className="mt-3">
             {/* Links */}
             <div className="flex items-center my-3">
               <div>
-                <img src={newsFeed} alt="" className="h-8 w-8" />
+                <img src={newsFeed} alt="" className="h-7 w-7" />
               </div>
               <span className="text-gray-500 font-semibold ml-2 hover:text-blue-600 transition-all duration-500 cursor-pointer">
                 Newsfeed
@@ -20,7 +22,7 @@ const NewFeeds = () => {
             </div>
             <div className="flex items-center my-3">
               <div>
-                <img src={badge} alt="" className="h-8 w-8" />
+                <img src={badge} alt="" className="h-7 w-7" />
               </div>
               <span className="text-gray-500 font-semibold ml-2 hover:text-blue-600 transition-all duration-500 cursor-pointer">
                 Badge
@@ -28,10 +30,26 @@ const NewFeeds = () => {
             </div>
             <div className="flex items-center my-3">
               <div>
-                <img src={storyIcon} alt="" className="h-8 w-8" />
+                <img src={storyIcon} alt="" className="h-7 w-7" />
               </div>
               <span className="text-gray-500 font-semibold ml-2 hover:text-blue-600 transition-all duration-500 cursor-pointer">
                 Explore Stories
+              </span>
+            </div>
+            <div className="flex items-center my-3">
+              <div>
+                <img src={popularIcon} alt="" className="h-7 w-7" />
+              </div>
+              <span className="text-gray-500 font-semibold ml-2 hover:text-blue-600 transition-all duration-500 cursor-pointer">
+                Popular Groups
+              </span>
+            </div>
+            <div className="flex items-center my-3">
+              <div>
+                <img src={userIcon} alt="" className="h-7 w-7" />
+              </div>
+              <span className="text-gray-500 font-semibold ml-2 hover:text-blue-600 transition-all duration-500 cursor-pointer">
+                Author Profile
               </span>
             </div>
           </div>
