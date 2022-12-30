9b28dadb3173ee665de988250522fffa4b8189bc
Sukanta Das <arnobh84@gmail.com>
2022-12-30 17:58:32 +0600

    user porfile page added

---
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <div>
+      <p>user Profile</p>
+    </div>
+  );
+};
+
+export default UserProfile;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import UserProfile from "../Pages/UserProfile/UserProfile";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/user-profile",
+        element: <UserProfile />,
       },
     ],
   },
