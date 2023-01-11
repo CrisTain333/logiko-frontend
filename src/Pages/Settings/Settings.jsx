0acf1434783c7dafa52995a62d98335d3e673b38
Sukanta Das <arnobh84@gmail.com>
2023-01-11 12:32:56 +0600

    account section added

--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import homeIcon from "../../assets/Icons/home.png";
 import locationIcon from "../../assets/Icons/placeholder.png";
 import socialIcon from "../../assets/Icons/social-media.png";
+import myCard from "../../assets/Icons/credit-card.png";
+import passwordIcon from "../../assets/Icons/lock.png";
 
 const Settings = () => {
   return (
@@ -41,6 +43,33 @@ const Settings = () => {
                 Social Account
               </span>
             </div>
+            <hr />
+          </div>
+        </div>
+
+        <div className="my-14">
+          <div className="links_div">
+            <p className="text-sm text-neutral font-medium">Account</p>
+          </div>
+
+          <div className="flex items-center  mt-5">
+            <div className="flex items-center justify-center">
+              <img src={myCard} alt="" className="h-9 w-9" />
+              <span className="text-sm font-semibold ml-3 hover:text-primary transition-all duration-500 cursor-pointer">
+                My Cards
+              </span>
+            </div>
+
+            <hr />
+          </div>
+          <div className="flex items-center  mt-5">
+            <div className="flex items-center justify-center">
+              <img src={passwordIcon} alt="" className="h-9 w-9" />
+              <span className="text-sm font-semibold ml-3 hover:text-primary transition-all duration-500 cursor-pointer">
+                Password
+              </span>
+            </div>
+
             <hr />
           </div>
         </div>
