5aba0641d7ced88244db13a4d138bbd8c9ec70c6
Sukanta Das <arnobh84@gmail.com>
2023-01-12 11:47:42 +0600

    auth context setup complete

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ThemeContext from "./context/ThemeContext";
+import AuthProvider from "./context/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeContext>
-    <App />
-  </ThemeContext>
+  <AuthProvider>
+    <ThemeContext>
+      <App />
+    </ThemeContext>
+  </AuthProvider>
 );
