e0a7db2074938a1f934505e8b8d16a30bfc0aa90
Sukanta Das <arnobh84@gmail.com>
2023-01-13 22:11:33 +0600

    singout user added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,7 +1,11 @@
 import { createContext, useState } from "react";
 import React from "react";
 import app from "../Firebase/Firebase.config";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -14,8 +18,18 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const singOutUser = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
-    <AuthContext.Provider value={{ createUser }}>
+    <AuthContext.Provider value={{ createUser, singOutUser }}>
       {children}
     </AuthContext.Provider>
   );
