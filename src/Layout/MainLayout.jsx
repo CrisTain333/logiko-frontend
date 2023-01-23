eed9c648d4ddf69fa7f6fbeb384dc9709b7f3528
Sukanta Das <arnobh84@gmail.com>
2023-01-23 14:20:19 +0600

    navbar changed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -26,12 +26,12 @@ const MainLayout = () => {
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* <!-- Page content here --> */}
-          <label
+          {/* <label
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button"
           >
             Open drawer
-          </label>
+          </label> */}
 
           <Outlet />
         </div>
